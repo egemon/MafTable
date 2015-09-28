353cdd77a5a3e80647d13989037329f3a9936bfc
Ilya Lykianov <lykianovib@gmail.com>
2015-09-29 00:38:27 +0300

    Added requirejs config

---
+++ js/requirejsConfig.js
@@ -0,0 +1,16 @@
+requirejs.config({
+    baseUrl: "js/",
+    paths: {
+        'microtemplates': 'lib/microtemplates/index',
+        'jquery': 'lib/jquery/dist/jquery',
+        'jquery-ui': 'lib/jquery-ui/jquery-ui.min',
+        'fs': 'lib/fs/dist/fs',
+        'text': 'lib/text/text',
+    },
+    optimizeAllPluginResources: true,
+    waitSeconds: 90
+    // "shim":{
+    //     'jquery': 'lib/jquery/dist/jquery',
+    //     'microtemplates': 'lib/microtemplates/index'
+    // }
+});
